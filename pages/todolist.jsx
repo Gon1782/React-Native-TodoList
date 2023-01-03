958d308047e93341ae879e3461251caa1a25c605
Gon <hss3522@gmail.com>
2023-01-03 18:24:42 +0900

    Docs: 안쓰는 코드 삭제

--- pages/todolist.jsx
+++ pages/todolist.jsx
@@ -2,9 +2,6 @@ import styled from "@emotion/native";
 import Tabs from "../components/Tabs";
 import Lists from "../components/Lists";
 import Forms from "../components/Forms";
-import { db } from "../firebase";
-import { query, collection, orderBy, getDocs, onSnapshot } from "firebase/firestore";
-import { useEffect, useState } from "react";
 
 const ToDoList = () => {
   return (
